a4e32023b6c0a4b8ce3f83e63ceef56229c6a5bb
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-20 19:52:14 +0300

    First

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldTarih: UITextField!
+    
+    var datePicker = UIDatePicker()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        textFieldTarih.inputView = datePicker
+        
+        
+        datePicker.addTarget(self, action: #selector(self.tarihGoster(datePicker:)), for: .valueChanged)
+        
+    }
+    
+    
+    @objc func tarihGoster(datePicker: UIDatePicker){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        textFieldTarih.text = dateFormatter.string(from: datePicker.date)
+        
+        print(datePicker.date)
     }
 
 
